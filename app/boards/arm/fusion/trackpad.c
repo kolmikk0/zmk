386205e2f8e9688b614b02f3f4b408c8b1a67ea6
crides <zhuhaoqing@live.cn>
2022-01-26 03:33:56 -0600

    sup f

--- app/boards/arm/fusion/trackpad.c
+++ app/boards/arm/fusion/trackpad.c
@@ -1,17 +1,21 @@
 #include <drivers/sensor.h>
 #include <logging/log.h>
 
+#include <zmk/hid.h>
+#include <zmk/endpoints.h>
+
 LOG_MODULE_REGISTER(trackpad, CONFIG_SENSOR_LOG_LEVEL);
 
 const struct device *trackpad = DEVICE_DT_GET(DT_INST(0, cirque_pinnacle));
 
 static void handle_trackpad(const struct device *dev, struct sensor_trigger *trig) {
+    static uint8_t last_pressed = 0;
     int ret = sensor_sample_fetch(dev);
     if (ret < 0) {
         LOG_ERR("fetch: %d", ret);
         return;
     }
-    struct sensor_value dx, dy, wheel;
+    struct sensor_value dx, dy, wheel, btn;
     ret = sensor_channel_get(dev, SENSOR_CHAN_POS_DX, &dx);
     if (ret < 0) {
         LOG_ERR("get dx: %d", ret);
@@ -27,7 +31,22 @@ static void handle_trackpad(const struct device *dev, struct sensor_trigger *tri
         LOG_ERR("get wheel: %d", ret);
         return;
     }
-    LOG_INF("(%d, %d) %d", dx.val1, dy.val1, wheel.val1);
+    ret = sensor_channel_get(dev, SENSOR_CHAN_PRESS, &btn);
+    if (ret < 0) {
+        LOG_ERR("get btn: %d", ret);
+        return;
+    }
+    zmk_hid_mouse_movement_set(0, 0);
+    zmk_hid_mouse_scroll_set(0, 0);
+    zmk_hid_mouse_movement_update(dx.val1, dy.val1);
+    zmk_hid_mouse_scroll_update(wheel.val1, 0);
+    if (!last_pressed && btn.val1) {
+        zmk_hid_mouse_buttons_press(1);
+    } else if (last_pressed && !btn.val1) {
+        zmk_hid_mouse_buttons_release(1);
+    }
+    zmk_endpoints_send_mouse_report();
+    last_pressed = btn.val1;
 }
 
 static int trackpad_init() {
