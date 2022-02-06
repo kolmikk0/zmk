99509a607bac14dbb39a94261038d062353730c0
crides <zhuhaoqing@live.cn>
2022-02-06 17:48:18 -0600

    scroll

--- app/boards/arm/fusion/trackpad.c
+++ app/boards/arm/fusion/trackpad.c
@@ -6,6 +6,8 @@
 #include <zmk/keymap.h>
 #include <dt-bindings/zmk/mouse.h>
 
+#define SCROLL_DIV_FACTOR 5
+
 LOG_MODULE_REGISTER(trackpad, CONFIG_SENSOR_LOG_LEVEL);
 
 const struct device *trackpad = DEVICE_DT_GET(DT_INST(0, cirque_pinnacle));
@@ -33,21 +35,28 @@ static void handle_trackpad(const struct device *dev, struct sensor_trigger *tri
         LOG_ERR("get btn: %d", ret);
         return;
     }
+    LOG_DBG("trackpad %d %d %02x", dx.val1, dy.val1, btn.val1);
     zmk_hid_mouse_movement_set(0, 0);
     zmk_hid_mouse_scroll_set(0, 0);
     const uint8_t layer = zmk_keymap_highest_layer_active();
     uint8_t button;
+    static uint8_t last_button = 0;
+    static int8_t scroll_ver_rem = 0, scroll_hor_rem = 0;
     if (layer == 1) {   // lower
-        zmk_hid_mouse_scroll_update(dx.val1, dy.val1);
+        const int16_t total_hor = dx.val1 + scroll_hor_rem, total_ver = -(dy.val1 + scroll_ver_rem);
+        scroll_hor_rem = total_hor % SCROLL_DIV_FACTOR;
+        scroll_ver_rem = total_ver % SCROLL_DIV_FACTOR;
+        zmk_hid_mouse_scroll_update(total_hor / SCROLL_DIV_FACTOR, total_ver / SCROLL_DIV_FACTOR);
         button = RCLK;
     } else {
-        zmk_hid_mouse_movement_update(dx.val1, dy.val1);
+        zmk_hid_mouse_movement_update(CLAMP(dx.val1, INT8_MIN, INT8_MAX), CLAMP(dy.val1, INT8_MIN, INT8_MAX));
         button = LCLK;
     }
     if (!last_pressed && btn.val1) {
         zmk_hid_mouse_buttons_press(button);
+        last_button = button;
     } else if (last_pressed && !btn.val1) {
-        zmk_hid_mouse_buttons_release(button);
+        zmk_hid_mouse_buttons_release(last_button);
     }
     zmk_endpoints_send_mouse_report();
     last_pressed = btn.val1;
