2e101c20f6b7398f0454b1262cb4eb6fbf937a05
crides <zhuhaoqing@live.cn>
2022-03-30 04:21:55 -0500

    buggy

--- app/boards/arm/fissure/board.cmake
+++ app/boards/arm/fissure/board.cmake
@@ -2,4 +2,3 @@
 
 board_runner_args(nrfjprog "--nrf-family=NRF52" "--softreset")
 include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
-include(${ZEPHYR_BASE}/boards/common/blackmagicprobe.board.cmake)
