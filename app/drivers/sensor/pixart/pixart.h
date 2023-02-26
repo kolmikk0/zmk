0ea3ef4debf11cd01ccadbbf5d9a59346d1c73f0
crides <zhuhaoqing@live.cn>
2023-02-26 01:07:56 -0600

    pmw

--- app/drivers/sensor/pixart/pixart.h
+++ app/drivers/sensor/pixart/pixart.h
@@ -17,7 +17,7 @@ extern "C" {
 #endif
 
 /* device data structure */
-  struct pixart_data {
+struct pixart_data {
     const struct device          *dev;
     int16_t                      x;
     int16_t                      y;
@@ -48,49 +48,49 @@ extern "C" {
 
     // for pmw3610 smart algorithm
     bool                         sw_smart_flag;
-  };
+};
 
-  // device config data structure
-  struct pixart_config {
+// device config data structure
+struct pixart_config {
     struct gpio_dt_spec irq_gpio;
+    struct gpio_dt_spec rst_gpio;
     struct spi_dt_spec bus;
     struct gpio_dt_spec cs_gpio;
-  };
+};
 
 /** @brief Sensor specific attributes of PIXART. */
 enum pixart_attribute {
-	/** Sensor CPI for both X and Y axes. */
-	PIXART_ATTR_CPI = SENSOR_ATTR_PRIV_START,
+    /** Sensor CPI for both X and Y axes. */
+    PIXART_ATTR_CPI = SENSOR_ATTR_PRIV_START,
 
-  /** Sensor CPI for X and Y axes. */
-  PIXART_ATTR_XCPI,
-  PIXART_ATTR_YCPI,
+    /** Sensor CPI for X and Y axes. */
+    PIXART_ATTR_XCPI,
+    PIXART_ATTR_YCPI,
 
-	/** Enable or disable sleep modes. */
-	PIXART_ATTR_REST_ENABLE,
+    /** Enable or disable sleep modes. */
+    PIXART_ATTR_REST_ENABLE,
 
-	/** Entering time from Run mode to REST1 mode [ms]. */
-	PIXART_ATTR_RUN_DOWNSHIFT_TIME,
+    /** Entering time from Run mode to REST1 mode [ms]. */
+    PIXART_ATTR_RUN_DOWNSHIFT_TIME,
 
-	/** Entering time from REST1 mode to REST2 mode [ms]. */
-	PIXART_ATTR_REST1_DOWNSHIFT_TIME,
+    /** Entering time from REST1 mode to REST2 mode [ms]. */
+    PIXART_ATTR_REST1_DOWNSHIFT_TIME,
 
-	/** Entering time from REST2 mode to REST3 mode [ms]. */
-	PIXART_ATTR_REST2_DOWNSHIFT_TIME,
+    /** Entering time from REST2 mode to REST3 mode [ms]. */
+    PIXART_ATTR_REST2_DOWNSHIFT_TIME,
 
-	/** Sampling frequency time during REST1 mode [ms]. */
-	PIXART_ATTR_REST1_SAMPLE_TIME,
+    /** Sampling frequency time during REST1 mode [ms]. */
+    PIXART_ATTR_REST1_SAMPLE_TIME,
 
-	/** Sampling frequency time during REST2 mode [ms]. */
-	PIXART_ATTR_REST2_SAMPLE_TIME,
+    /** Sampling frequency time during REST2 mode [ms]. */
+    PIXART_ATTR_REST2_SAMPLE_TIME,
 
-	/** Sampling frequency time during REST3 mode [ms]. */
-	PIXART_ATTR_REST3_SAMPLE_TIME,
+    /** Sampling frequency time during REST3 mode [ms]. */
+    PIXART_ATTR_REST3_SAMPLE_TIME,
 
-  /** Select the running mode. */
-  PIXART_ATTR_RUN_MODE,
+    /** Select the running mode. */
+    PIXART_ATTR_RUN_MODE,
 };
-
 
 #ifdef __cplusplus
 }
